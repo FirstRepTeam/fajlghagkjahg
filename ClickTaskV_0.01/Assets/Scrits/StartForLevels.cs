ee0f7be8ef747356b4ba219b2c35ef7d163962c5
MrWhite\Mr.White <xenus7@inbox.ru>
2016-10-12 15:26:42 +0300

    Add map and levels

---
+++ ClickTaskV_0.01/Assets/Scrits/StartForLevels.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartForLevels : MonoBehaviour {
+
+    
+    public GameObject _levelPanel,_mapPanel;
+
+    public void ShowLevel()
+    {
+        _levelPanel.SetActive(true);
+        _mapPanel.SetActive(false);
+    }
+
+    
+
+}
