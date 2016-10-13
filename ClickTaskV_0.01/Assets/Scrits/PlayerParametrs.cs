d22f91e2731be65394559b7f694bf683567f94c6
MrWhite\Mr.White <xenus7@inbox.ru>
2016-10-13 23:24:37 +0300

    Add monster params coeficients/

--- ClickTaskV_0.01/Assets/Scrits/PlayerParametrs.cs
+++ ClickTaskV_0.01/Assets/Scrits/PlayerParametrs.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using UnityEngine.UI;
 public class PlayerParametrs : MonoBehaviour {
-
+    private float _monsterArmor=10;//10 percent
+    private float _monsterMagicresist = 10;
     [SerializeField]
     private Text _strengthCount, _sleightCount, _intellectCount, _staminaCount;
 
@@ -10,24 +11,20 @@ public class PlayerParametrs : MonoBehaviour {
     private float _strength, _agility, _intellect, _stamina,_SAICoef; //Main states 
     
     [SerializeField]
-    private float _basicClickDamage, _basicCritChance,_basicCritPowerCoef,_basicSkillsPowerCoef,_basicColdounCoef,_basicResistCoef;
+    private float _basicClickDamage=1, _basicCritChance=10,_basicCritPowerCoef=1.5f,_basicSkillsPowerCoef=1,_basicColdounCoef=1,_basicResistCoef=0;
 
     [SerializeField]
     private float LvL_Score;
     //Game params
     [SerializeField]
-    private float _clotheStrength, _clotheAgility, _clotheIntellect, _clotheStamina;//Clothes prams
+    private float _clotheStrength=0, _clotheAgility=0, _clotheIntellect=0, _clotheStamina=0,_clotheMultiplyCriticalPowerChance=1.5f,_clotheMultiplyColdounCoef=1;//Clothes prams
 
     [SerializeField]
-    private float _passiveModStrength, _passiveModAgility, _passiveModintellect, _passiveModStamina;
+    private float _passiveModStrength=1, _passiveModAgility=1, _passiveModintellect=1, _passiveModStamina=1, _passiveMultiplierCriticalChanceCoef=1 , _passiveMultiplierRollbackCoolDown=1;
+
     //Passive modification without 2 coefs;
     [SerializeField]
     private float _passiveLvlExecutioner, _passiveLvlrush, _passiveLvlmagicArmor;
-
-    
-    
-
-
     [HideInInspector]
     public float timeDecreaseCoeficient = 0.1f;
 
@@ -46,15 +43,11 @@ public class PlayerParametrs : MonoBehaviour {
     // calculated params
     public void CalulateParams()
         {
-
         //params
         _strength = System.Convert.ToSingle(_strengthCount);
         _agility = System.Convert.ToSingle(_sleightCount);
         _intellect = System.Convert.ToSingle(_intellectCount);
         _stamina = System.Convert.ToSingle(_staminaCount);
-  
-
-
         float _spendingStregth  = 2 * _passiveLvlExecutioner;
         float _spendingAgility = 2 * _passiveLvlrush;
         float _spendingIntellect = 2 * _passiveLvlmagicArmor;
@@ -66,6 +59,8 @@ public class PlayerParametrs : MonoBehaviour {
         float _finalStateAgility = (_agility + _clotheAgility) * _passiveModAgility;
         float _finalStateIntellect = (_intellect + _clotheIntellect) * _passiveModintellect;
         float _finalStateStaina = (_stamina + _clotheStamina) * _passiveModStamina;
+        float _finalMultiCritPowerCoef = (_clotheMultiplyCriticalPowerChance * _passiveMultiplierCriticalChanceCoef);
+        float _finalMultiplierRollbackCoolDown = (_passiveMultiplierRollbackCoolDown*_clotheMultiplyColdounCoef);
         float _clickCritcoef=1+(0.2f*_passiveLvlExecutioner);
         float _clickDamage = (_basicClickDamage + _finalStateStrength) * _clickCritcoef;
         float _effectOnCritChance = 3 * _basicCritChance * _finalStateAgility;
@@ -74,14 +69,14 @@ public class PlayerParametrs : MonoBehaviour {
         float _skillPowercoef = _basicSkillsPowerCoef+(_finalStateIntellect/(_koefB15/(_SAICoef)));
         float _coldounSkillCoef = 1;
         float _resistCoef = _basicResistCoef + (_koefB15 / _finalStateStaina);
-
+        //Monsters
+        float _monsterDamageEffect = 1- (_passiveLvlmagicArmor*0.05f);
+        float _monsterDamage = ((1 + _expForLevel / 50) / 2) * _monsterDamageEffect;
+        float _monsterHealth =_koefB15*_heroLevel*1.5f;
+        float _monsterExp = (_monsterHealth + _expForLevel / (_koefB15 * _heroLevel))/15;
+        float _monsterGold = ((_monsterHealth + _expForLevel / (_heroLevel * _koefB15)) / 15) * _heroLevel;
+        float _monsterDropCoef = (_monsterHealth + _expForLevel / (_koefB15 * _heroLevel)) / 15;
     }   
-
-
-
-
-
-
     public void setNormalTimeDecrease()
     {
         timeDecreaseCoeficient = 0.1f;
@@ -114,9 +109,4 @@ public class PlayerParametrs : MonoBehaviour {
         else
             return 1.0f;
     }
-
-
-
-
-
 }
