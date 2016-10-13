6237fd6d5011df359d0527a55668ffcc22a2fdb5
MrWhite\Mr.White <xenus7@inbox.ru>
2016-10-13 21:51:40 +0300

    add params the table

--- ClickTaskV_0.01/Assets/Scrits/PlayerParametrs.cs
+++ ClickTaskV_0.01/Assets/Scrits/PlayerParametrs.cs
@@ -3,6 +3,30 @@ using System.Collections;
 using UnityEngine.UI;
 public class PlayerParametrs : MonoBehaviour {
 
+    [SerializeField]
+    private Text _strengthCount, _sleightCount, _intellectCount, _staminaCount;
+
+    [SerializeField]
+    private float _strength, _agility, _intellect, _stamina,_SAICoef; //Main states 
+    
+    [SerializeField]
+    private float _basicClickDamage, _basicCritChance,_basicCritPowerCoef,_basicSkillsPowerCoef,_basicColdounCoef,_basicResistCoef;
+
+    [SerializeField]
+    private float LvL_Score;
+    //Game params
+    [SerializeField]
+    private float _clotheStrength, _clotheAgility, _clotheIntellect, _clotheStamina;//Clothes prams
+
+    [SerializeField]
+    private float _passiveModStrength, _passiveModAgility, _passiveModintellect, _passiveModStamina;
+    //Passive modification without 2 coefs;
+    [SerializeField]
+    private float _passiveLvlExecutioner, _passiveLvlrush, _passiveLvlmagicArmor;
+
+    
+    
+
 
     [HideInInspector]
     public float timeDecreaseCoeficient = 0.1f;
@@ -18,36 +42,44 @@ public class PlayerParametrs : MonoBehaviour {
     private float correctiveHitStrangth = 1.0f;
 
     [SerializeField]
-    private Slider fireS, airS, waterS, earthS;
-    [SerializeField]
-    private Text fireT, airT, waterT, earthT;
-    [SerializeField]
-    private GameObject Skills;
+    private float newPassiveSpending=0;
+    // calculated params
+    public void CalulateParams()
+        {
+
+        //params
+        _strength = System.Convert.ToSingle(_strengthCount);
+        _agility = System.Convert.ToSingle(_sleightCount);
+        _intellect = System.Convert.ToSingle(_intellectCount);
+        _stamina = System.Convert.ToSingle(_staminaCount);
+  
+
+
+        float _spendingStregth  = 2 * _passiveLvlExecutioner;
+        float _spendingAgility = 2 * _passiveLvlrush;
+        float _spendingIntellect = 2 * _passiveLvlmagicArmor;
+        float _charapterParams = _spendingStregth + _spendingAgility + _spendingIntellect + newPassiveSpending;
+        float _koefB15 = (_strength + _agility + _intellect + _stamina + _charapterParams)/4;
+        float _heroLevel = 1 + ((_strength + _agility + _intellect + _stamina) - 40) / LvL_Score;
+        float _expForLevel = 100 * (_heroLevel * _heroLevel);
+        float _finalStateStrength = (_strength + _clotheStrength) * _passiveModStrength;
+        float _finalStateAgility = (_agility + _clotheAgility) * _passiveModAgility;
+        float _finalStateIntellect = (_intellect + _clotheIntellect) * _passiveModintellect;
+        float _finalStateStaina = (_stamina + _clotheStamina) * _passiveModStamina;
+        float _clickCritcoef=1+(0.2f*_passiveLvlExecutioner);
+        float _clickDamage = (_basicClickDamage + _finalStateStrength) * _clickCritcoef;
+        float _effectOnCritChance = 3 * _basicCritChance * _finalStateAgility;
+        float _critChance = (_basicCritChance + (_finalStateAgility / (_koefB15 * _SAICoef))) + _effectOnCritChance;
+        float _critStrengthCoef = 1.5f;
+        float _skillPowercoef = _basicSkillsPowerCoef+(_finalStateIntellect/(_koefB15/(_SAICoef)));
+        float _coldounSkillCoef = 1;
+        float _resistCoef = _basicResistCoef + (_koefB15 / _finalStateStaina);
+
+    }   
 
 
 
-    public float CalculateSkills()
-    {
-        float damage = 0f;
-        damage = (fireS.value - waterS.value) +
-        (fireS.value + airS.value) +
-        (waterS.value + earthS.value) +
-        (earthS.value - airS.value);
-        return damage;
-    }
 
-
-    public void SkillsCount()
-    {
-        fireT.text = fireS.value.ToString();
-        airT.text = airS.value.ToString();
-        waterT.text = waterS.value.ToString();
-        earthT.text = earthS.value.ToString();
-    }
-    public void OnOffSkill()
-    {
-        Skills.SetActive(!Skills.activeSelf);
-    }
 
 
     public void setNormalTimeDecrease()
