38c3f901d7497909eeb01503af45dd486b385ffe
MrWhite\Mr.White <xenus7@inbox.ru>
2016-10-14 16:50:06 +0300

    log in Server

--- ClickTaskV_0.01/Assets/Scrits/ServerConnect.cs
+++ ClickTaskV_0.01/Assets/Scrits/ServerConnect.cs
@@ -3,16 +3,25 @@ using System.Collections;
 using com.shephertz.app42.paas.sdk.csharp;
 using com.shephertz.app42.paas.sdk.csharp.user;
 using System;
-
+using UnityEngine.UI;
 public class ServerConnect: MonoBehaviour {
-    string userName = "GameLeadMmrBank";
-    string pwd = "ujlfytrhvkz111";
-    string emailId = "bogdanlikhoglyad@gmail.com";
+    public Text _inputLogin, _inputPassword, _inputMail;
+
     UserService userService = App42API.BuildUserService();
- 
+
+    string userName, pwd,emailId;
+
+    public void Registr() {
+
+         userName = _inputLogin.text;   //= "GameLeadMmrBank";
+         pwd = _inputPassword.text; //= "ujlfytrhvkz111";
+         emailId = _inputMail.text; //= "bogdanlikhoglyad@gmail.com";
+
+    }
 
     public void ServerWorking()
     {
+        
         App42API.Initialize("c53587259f23dd894259dfa9b7ac90e7dca48a3b9e2345f372bd6bd3b826e2d0","0029c09894c20fe1d28bade219995e1204f24a4cbd407e725eb4271a5e29e122");
         userService.CreateUser(userName, pwd, emailId, new UnityCallBack());
     }
