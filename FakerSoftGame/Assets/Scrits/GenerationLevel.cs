e5ce55567f6183f8647e8b0e39b61470e2dd5a82
Alex <alexopti38@gmail.com>
2016-10-31 21:19:06 +0200

    Add: tavern cut, level generation, map

---
+++ FakerSoftGame/Assets/Scrits/GenerationLevel.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+using UnityEngine.SceneManagement;
+
+public class GenerationLevel : MonoBehaviour {
+    int level;
+    public GameObject levelUI;
+    string[] levels = { "Вялый лес", "Дмечуе пески", "Сливная пустыня" };
+    public Image background;
+    public Image[] level_pointer;
+    double forlevel;
+    public Text getlevel;
+
+
+    void TakeInfAboutLevel()
+    {
+        //Получаем с базы колличество прошедшых уровней в данной области(регионе) по ключу из массива и устанавливаем background
+        if (getlevel.text == null) level = 1;
+        else
+        {
+            level = Convert.ToInt32(getlevel.text, 16);
+        }
+    }
+
+    public void LevelGeneration()
+    {
+        levelUI.SetActive(true);
+        TakeInfAboutLevel();
+        forlevel = level % level_pointer.Length;
+        for (int i = 0; i < level_pointer.Length; i++)
+        {
+            if (i > forlevel) level_pointer[i].color = new Color32(96, 96, 96, 255);
+            else
+            {
+                if (i == forlevel) { level_pointer[i].color = new Color32(255, 50, 255, 255); }//+делаем какую то подсветку, нам на этот уровень
+                else level_pointer[i].color = new Color32(255, 255, 255, 255);
+            }
+        }
+    }
+
+    public void StartFight()
+    {
+        SceneManager.LoadScene("clickScene");
+    }
+}
+
---
+++ FakerSoftGame/Assets/Scrits/UISwitcher.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UISwitcher : MonoBehaviour {
+
+    public GameObject[] buttons;
+    public GameObject[] backseens;
+    bool[] buttonsState = { true, true,true,true,true};
+
+    public void PlayerButtoOn()
+    {
+        if (buttonsState[0] == true)
+        {
+            CloseButtons();
+            buttonsState[0] = false; 
+            backseens[0].SetActive(false);
+            backseens[1].SetActive(false);
+            backseens[2].SetActive(true);
+            backseens[3].SetActive(false);
+            backseens[4].SetActive(false);
+            buttons[5].SetActive(true);
+        }
+        else {
+            CloseButtons();
+        };
+
+
+    }
+
+    public void BagButtinOb() {
+        if (buttonsState[1] == true)
+        {
+            CloseButtons();
+            buttonsState[1] = false;
+            backseens[0].SetActive(false);
+        backseens[1].SetActive(false);
+        backseens[2].SetActive(true);
+        backseens[3].SetActive(false);
+        backseens[4].SetActive(true);
+        buttons[5].SetActive(true);
+        }
+        else
+        {
+            CloseButtons();
+        };
+    }
+
+    public void MapButtonOn() {
+        if (buttonsState[2] == true)
+        {
+            CloseButtons();
+            buttonsState[2] = false;
+            backseens[0].SetActive(false);
+        backseens[1].SetActive(true);
+        backseens[2].SetActive(false);
+        backseens[3].SetActive(false);
+        backseens[4].SetActive(false);
+        buttons[5].SetActive(false);
+        }
+        else
+        {
+            CloseButtons();
+        };
+    }
+
+    public void SettingButtonOn() { }
+
+    public void ShopButtonOn() {
+        if (buttonsState[4] == true)
+        {
+            CloseButtons();
+            buttonsState[4] = false;
+            backseens[0].SetActive(true);
+        backseens[1].SetActive(false);
+        backseens[2].SetActive(false);
+        backseens[3].SetActive(false);
+        backseens[4].SetActive(false);
+        buttons[5].SetActive(false);
+        }
+        else
+        {
+            CloseButtons();
+        };
+    }
+
+    public void SkillButtonOn() {
+      
+            backseens[0].SetActive(false);
+        backseens[1].SetActive(false);
+        backseens[2].SetActive(false);
+        backseens[3].SetActive(true);
+        backseens[4].SetActive(false);
+        buttons[5].SetActive(true);
+       
+    }
+
+ 
+    void CloseButtons()
+    {
+        buttonsState[0] = true;
+        buttonsState[1] = true;
+        buttonsState[2] = true;
+        buttonsState[3] = true;
+        buttonsState[4] = true;
+        backseens[0].SetActive(false);
+        backseens[1].SetActive(false);
+        backseens[2].SetActive(false);
+        backseens[3].SetActive(false);
+        backseens[4].SetActive(false);
+        backseens[5].SetActive(false);
+        buttons[5].SetActive(false);
+    }
+}
