53444e9960b19b47554e1bbac363f03a3ccae49d
LAPTOP-R8B2GPLT\Lenovo <bogdanlikhoglyad@gmail.com>
2017-04-21 21:10:45 +0300

    Autintification

---
+++ FakerSoftGame/Assets/Scrits/Aut.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using com.shephertz.app42.paas.sdk.csharp.user;
+using com.shephertz.app42.paas.sdk.csharp;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+public class Aut : MonoBehaviour {
+
+  
+    public InputField _inputLoginAut, _inputPasswordAut;
+    public String userNameAut;
+    public String pwdAut;
+    UserService userService = App42API.BuildUserService();
+
+    public void aut()
+    {
+        userNameAut = _inputLoginAut.text;
+           pwdAut = _inputPasswordAut.text;  
+        userService.Authenticate(userNameAut, pwdAut, new UnityCallBack());
+
+    }
+    public class UnityCallBack : App42CallBack
+    {
+        public void OnSuccess(object response)
+        {
+            User user = (User)response;
+            App42Log.Console("userName is " + user.GetUserName());
+            App42Log.Console("sessionId is " + user.GetSessionId());
+            Debug.Log("+++++");
+            Debug.Log(user.GetUserName());
+        }
+        public void OnException(Exception e)
+        {
+            App42Log.Console("Exception : " + e);
+            Debug.Log("-----");
+
+        }
+    }
+
+}
--- FakerSoftGame/Assets/Scrits/Server/UserResponce.cs
+++ FakerSoftGame/Assets/Scrits/Server/UserResponce.cs
@@ -10,10 +10,26 @@ namespace AssemblyCSharp
 public class UserResponce : MonoBehaviour
 {
         public InputField _inputLogin, _inputPassword, _inputEnailId;
-        String userName ;
+        public InputField _inputLoginAut, _inputPasswordAut;
+        String userName;
         String pwd ;
-        String emailId ;
+        String emailId= "gsj@gmail.com" ;
 
+
+        UserService userService = App42API.BuildUserService();
+     
+       
+         
+       
+        public void aut  ()
+        {
+            ServiceAPI api = new ServiceAPI("37be471516e78a3693e7ca3ad228a8d73d9a55a9667b50838d97edd3dd80ee99", "4d70deecc103dccd19a3ad383c0f2949dd99abafa6a60cf23b4be995263c415c");
+            UserService userService = api.BuildUserService();
+            userName = _inputLoginAut.text;
+            pwd = _inputPasswordAut.text;
+            userService.Authenticate(userName, pwd, new UnityCallBack());
+           
+        }
         public void SrtvrtWork ()
         {
             userName = _inputLogin.text;
@@ -22,9 +38,10 @@ public class UserResponce : MonoBehaviour
             ServiceAPI api = new ServiceAPI("37be471516e78a3693e7ca3ad228a8d73d9a55a9667b50838d97edd3dd80ee99", "4d70deecc103dccd19a3ad383c0f2949dd99abafa6a60cf23b4be995263c415c");
             UserService userService = api.BuildUserService();
             userService.CreateUser(userName, pwd, emailId, new UnityCallBack());
+           
         }
         
-
+            
         
         
         public class UnityCallBack : App42CallBack
@@ -32,61 +49,16 @@ public class UserResponce : MonoBehaviour
             public void OnSuccess(object response)
             {
                 User user = (User)response;
-                /* This will create user in App42 cloud and will return User object */
                 App42Log.Console("userName is " + user.GetUserName());
-                App42Log.Console("emailId is " + user.GetEmail());
+                App42Log.Console("sessionId is " + user.GetSessionId());
+                Debug.Log("+++++++++");
             }
             public void OnException(Exception e)
             {
                 App42Log.Console("Exception : " + e);
+                Debug.Log("-------"+e);
             }
         }
-
-        private string result = "";
-    public void OnSuccess(object user)
-    {
-        try
-        {
-            if (user is User)
-            {
-                User userObj = (User)user;
-                result = userObj.ToString();
-                Debug.Log("UserName : " + userObj.GetUserName());
-                Debug.Log("EmailId : " + userObj.GetEmail());
-                User.Profile profileObj = (User.Profile)userObj.GetProfile();
-                if (profileObj != null)
-                {
-                    Debug.Log("FIRST NAME" + profileObj.GetFirstName());
-                    Debug.Log("SEX" + profileObj.GetSex());
-                    Debug.Log("LAST NAME" + profileObj.GetLastName());
-                }
-            }
-            else
-            {
-                IList<User> userList = (IList<User>)user;
-                result = userList[0].ToString();
-                Debug.Log("UserName : " + userList[0].GetUserName());
-                Debug.Log("EmailId : " + userList[0].GetEmail());
-
-            }
-        }
-        catch (App42Exception e)
-        {
-            result = e.ToString();
-            Debug.Log("App42Exception : " + e);
-        }
-    }
-
-    public void OnException(Exception e)
-    {
-        result = e.ToString();
-        Debug.Log("Exception : " + e);
-    }
-
-    public string getResult()
-    {
-        return result;
-    }
 }
 }
 
