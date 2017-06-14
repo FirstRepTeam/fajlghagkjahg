4e13f4a9e3ede399039e130c8a6bbda7e11e8284
Kilio7 <kilio94@qip.ru>
2017-06-14 03:22:14 +0300

    project fix

---
+++ FakerSoftGame/Assets/Scripts/UI/in Work (dark)/tabFogOnMouse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+ 
+public class tabFogOnMouse : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler{
+void IPointerEnterHandler.OnPointerEnter (PointerEventData eventData){
+this.GetComponent<Image>().color = new Color32(150,150,150,100);
+}
+void IPointerExitHandler.OnPointerExit (PointerEventData eventData){
+GetComponent<Image>().color = new Color32(255,255,255,255);
+}
+}
---
+++ FakerSoftGame/Assets/Scripts/UI/in Work (dark)/tabSwitch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class tabSwitch : MonoBehaviour, IPointerClickHandler {
+private GameObject skills;
+private GameObject inv;
+private GameObject stats;
+private string currentTab;
+private string teststr;
+void Start(){
+	currentTab = gameObject.name;
+	skills = transform.parent.parent.Find("content").Find("skills").gameObject;
+	inv = transform.parent.parent.Find("content").Find("inv").gameObject;
+	stats = transform.parent.parent.Find("content").Find("stats").gameObject;
+	// Debug.Log("skill tab = "+skills.name);
+}
+/*
+если куртаб = таблефт
+то вырубить инвентарь и статсы
+
+
+*/
+void IPointerClickHandler.OnPointerClick(PointerEventData eventData){
+	if(currentTab == "Tab_left"){
+		inv.SetActive(false);
+		stats.SetActive(false);
+		skills.SetActive(true);
+	}
+	if(currentTab == "Tab_middle"){
+		inv.SetActive(true);
+		stats.SetActive(false);
+		skills.SetActive(false);
+	}
+	if(currentTab == "Tab_right"){
+		inv.SetActive(false);
+		stats.SetActive(true);
+		skills.SetActive(false);
+	}
+}
+}
